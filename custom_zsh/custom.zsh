7974be682b98c807dc211099c525b9310db71750
Manuel Vogel <vogel@stocard.de>
2017-12-11 17:12:37 +0200

    Set editor var to vim.

--- custom_zsh/custom.zsh
+++ custom_zsh/custom.zsh
@@ -48,6 +48,7 @@ export JAVA_HOME=$(/usr/libexec/java_home)
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128m"
 export GOPATH=$HOME/Development/go
 export GOBIN=$GOPATH/bin
+export EDITOR=vim
 
 # PATH
 PATH=$PATH:$GOPATH/bin
